9a20152b0fad67d7e86d152772c45d241545b008
Shubhangi <shubhangi@teewe.in>
2017-04-13 14:53:47 +0530

    adding components

---
+++ reduxtagram/client/components/Main.js
@@ -0,0 +1,22 @@
+import React, { Component }  from 'react';
+
+import {Link} from 'react-router';
+
+class Main extends Component {
+	render(){
+		return(
+			<div>
+				<h1>
+					<Link to="/">Reduxstagram</Link>
+				</h1>
+				{React.cloneElement(this.props.children, this.props)}
+			</div>
+		);
+	}
+}
+
+export default Main;
+
+
+
+// {this.props.children} is going to be populated by react-router
